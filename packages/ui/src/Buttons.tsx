e4e39d1ab6a3225fc6a7e079d8d5e8894f7fb6d8
younghoon_CHOI_^ <bi9choi@gmail.com>
2023-01-13 17:31:16 +0900

    <추가> : <모노레포 구성 초안>

---
+++ packages/ui/src/Buttons.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+
+export const Buttons = () => {
+  useEffect(() => {
+    console.log("button");
+    console.log("button");
+  }, []);
+  return <div>Buttons</div>;
+};
---
+++ packages/ui/vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'index',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          'react': 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+  plugins: [react(), dts(), tsconfigPaths()],
+});
